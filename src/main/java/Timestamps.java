1cfa923922d27fe0b621090b490858fc0a28334c
pandab3arr <70656430+pandab3arr@users.noreply.github.com>
2020-09-11 14:30:01 -0700

    Update Timestamps.java

--- src/main/java/Timestamps.java
+++ src/main/java/Timestamps.java
@@ -13,12 +13,20 @@ class Timestamps {
         int hours2 = scanner.nextInt();
         int minutes2 = scanner.nextInt();
         int seconds2 = scanner.nextInt();
-
+        int deltaH;
+        int deltaM;
+        int deltaS;
+        int secPast;
 
         /*
-         *  your code goes here
+         *  Finds the seconds that went by through the given time frame by finding delta hours, delta minutes and delta seconds and then adding them all up after everything gets converted into seconds.
          */
 
+        deltaH = hours2 - hours1;
+        deltaM = minutes2 - minutes1;
+        deltaS = seconds2 - seconds1;
+        secPast = (deltaH * 3600) + (deltaM * 60) + (deltaS);
+        System.out.print(secPast);
 
         // closing the scanner object
         scanner.close();
