5c13377320f07ed9eb1921d62a0190f69a2b0d30
pandab3arr <70656430+pandab3arr@users.noreply.github.com>
2020-09-11 14:28:01 -0700

    Update AnalogClock.java

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -7,11 +7,22 @@ class AnalogClock {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-
+        int time = scanner.nextInt();
+        int minPerDeg = 2;
+        int degPerMin = 6;
+        int minPast;
+        int minHandDeg;
 
         /*
-         *  your code goes here
+         *  Finds the number of minutes past and converts it
+         *  into how many degrees the hand would pass on a Clock
+         *  by using the formula for every 10 minutes, 60 degrees 
+         *  pass on a minute hand 
          */
+        minPast = time % 60;
+        minPast = minPast*minPerDeg;
+        minHandDeg = minPast*degPerMin;
+        System.out.print(minHandDeg);
 
         // closing the scanner object
         scanner.close();
